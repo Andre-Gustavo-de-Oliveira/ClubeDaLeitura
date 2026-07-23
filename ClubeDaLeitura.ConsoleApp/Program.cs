ccd743f217a3b72f6362f7e5ee292e342ad87c13
André Oliveira <andre.oliveira.contato@outlook.com>
2026-07-23 20:58:17 -0300

    Adiciona  validação e visualização de empretimo de amigos e Validação de status de Emprestimo e Revista

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -13,6 +13,7 @@ Caixa caixaTeste = new Caixa("Ação", "Vermelho", 5);
 Revista revistaTeste = new Revista("Action Comics", 1, 1976, caixaTeste);
 Amigo amigoTeste = new Amigo("Junior Testes", "Seu Oswaldo", "49988884444");
 Emprestimo emprestimoTeste = new Emprestimo(revistaTeste, amigoTeste);
+emprestimoTeste.Abrir();
 
 repositorioCaixa.Cadastrar(caixaTeste);
 repositorioRevista.Cadastrar(revistaTeste);
@@ -21,7 +22,7 @@ repositorioEmprestimo.Cadastrar(emprestimoTeste);
 
 TelaCaixa telaCaixa = new TelaCaixa("Caixa", repositorioCaixa, repositorioRevista);
 TelaRevista telaRevista = new TelaRevista("Revista", repositorioRevista, repositorioCaixa);
-TelaAmigo telaAmigo = new TelaAmigo("Amigo", repositorioAmigo);
+TelaAmigo telaAmigo = new TelaAmigo("Amigo", repositorioAmigo, repositorioEmprestimo);
 TelaEmprestimo telaEmprestimo = new TelaEmprestimo(
     repositorioEmprestimo,
     repositorioRevista,
@@ -100,6 +101,9 @@ while (true)
 
             else if (opcaoMenuInterno == "4")
                 telaAmigo.VisualizarTodos(true);
+
+            else if (opcaoMenuInterno == "5")
+                telaAmigo.VisualizarEmprestimosAmigo();
         }
 
         else if (opcaoMenuPrincipal == "4") // Empréstimos
