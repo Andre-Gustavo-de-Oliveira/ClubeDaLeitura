5a2be7d7aaebca0f80f4c008d630d34a71338893
André Oliveira <andre.oliveira.contato@outlook.com>
2026-07-23 19:52:31 -0300

    Adicionado Tela principal, Tratamento de Opções e validação de exclusão de Caixa!

--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/Caixa.cs
@@ -2,7 +2,16 @@ using ClubeDaLeitura.ConsoleApp.Utilidades;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa;
 
+/*
+    ● Campos obrigatórios:
+        ○ Etiqueta (texto único, máximo 50 caracteres)
+        ○ Cor (seleção de paleta ou hexadecimal)
+        ○ Dias de empréstimo (número, padrão 7)
 
+    ● Não pode haver etiquetas duplicadas
+    ● Não permitir excluir uma caixa caso tenha revistas vinculadas
+    ● Cada caixa define o prazo máximo para empréstimo de suas revistas
+*/
 public class Caixa
 {
     public int Id { get; private set; }
@@ -10,7 +19,8 @@ public class Caixa
     public string Cor { get; private set; }
     public int DiasDeEmprestimo { get; private set; }
 
-
+    // Construtor de Classe
+    // Toda instância que for criada PRECISA conter essas informações
     public Caixa(string etiqueta, string cor, int diasDeEmprestimo)
     {
         Id = GeradorIds.ObterIdCaixa();
